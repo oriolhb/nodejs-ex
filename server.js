4c8570ee686716ffc477b244016554968fa9b33f
Oriol Huguet <oriolhuguet15@gmail.com>
2017-06-18 18:38:38 +0200

    Primera versió de prova (no rep paràmetres)

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var sha = require('sha.js');
+var hash1_pass = sha.hex_hmac_sha256('$1$SERCOMM$', 'test123');
+var user_password = sha.hex_hmac_sha256("6E9925BE5119D153", hash1_pass);
 var server = http.createServer();
 function control(petic, resp) {
 resp.writeHead(200, {'content-type': 'text/plain'});
-resp.write('Hola, Mundo!');
+resp.write(user_password);
 resp.end();
 }
 server.on('request', control);
